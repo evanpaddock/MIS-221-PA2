a143a07c0fd13ba153f11d9188223d4a119ac5a7
Evan <112584442+evanpaddock@users.noreply.github.com>
2022-09-15 15:56:39 -0500

    Selection Menu

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿//menu
+﻿//Program Main
 string userName = GetUserName();
  
 int userSelection = SelectionMenu();
 
 UserSelectionVerify(userSelection, userName);
+
+BreakIntoProgram(userSelection);
  
-//methods
+//Main Methods
 static string GetUserName()
 {
     System.Console.WriteLine("What is your name?");
@@ -19,12 +21,12 @@ static int SelectionMenu()
     System.Console.WriteLine("");
     System.Console.WriteLine("Press \"1\" for Budget Calulator");
     System.Console.WriteLine("Press \"2\" for Currency Converter");
-    System.Console.WriteLine("Type \"Exit\" to exit");
+    System.Console.WriteLine("Press \"3\" to exit");
     System.Console.WriteLine("");
     System.Console.WriteLine("Then press enter");
 
     int userSelection = int.Parse(Console.ReadLine());
-    
+
     return (userSelection);
 }
 static void UserSelectionVerify(int userSelection, string userName)
@@ -34,6 +36,40 @@ static void UserSelectionVerify(int userSelection, string userName)
     if( controlInt == -1)
     {
         System.Console.WriteLine($"{userName} please try again");
+        SelectionMenu();
     }
-    SelectionMenu();
+    
+}
+static void BreakIntoProgram(int userSelection)
+{
+    switch(userSelection)
+    {
+        case 1:
+        System.Console.WriteLine("You are now using the Budget Calculator");
+        BudgetCalculator();
+        break;
+
+        case 2:
+        System.Console.WriteLine("You are now using the Currency Converter");
+        CurrencyConverter();
+        break;
+
+        case 3:
+        System.Console.WriteLine("You are now exiting the program");
+        break;
+    }
+}
+//Program Methods
+static void BudgetCalculator()
+{
+    System.Console.WriteLine("This is the Budget Calculator");
+
+}
+static void CurrencyConverter()
+{
+    System.Console.WriteLine("Ths is the Currency Converter");
+}
+static void ProgramExit()
+{
+    System.Console.WriteLine("The program is now closing");
 }
