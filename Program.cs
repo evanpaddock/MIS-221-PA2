06d54ea7bf703c4b5f3222e7cd4d3fb64af9a506
Evan <112584442+evanpaddock@users.noreply.github.com>
2022-09-14 22:34:15 -0500

    9-14 Menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//menu
+GetUserName();
+ 
+RunProgram();
+
+//methods
+static void GetUserName()
+{
+    System.Console.WriteLine("What is your name?");
+    string userName = Console.ReadLine();
+    System.Console.WriteLine($"Hello {userName} what would you like to do today?");
+}
+static void RunProgram()
+{
+    System.Console.WriteLine("Press \"1\" for Budget Calulator | Press \"2\" for Currency Converter | Press \"3\" for Budget Calulator. Then press enter.");
+    int userSelection = int.Parse(Console.ReadLine());
+
+    System.Console.WriteLine($"You chose {userSelection} is this correct? If not enter -1");
+    int controlInt = int.Parse(Console.ReadLine());
+
+    if(controlInt == -1 )
+    {
+        System.Console.WriteLine("Please try again!");
+    }
+    else if (userSelection == 1)
+    {
+        System.Console.WriteLine("You chose option \"1\".");
+        Console.ReadLine();
+    }
+    else if (userSelection == 2)
+    {
+        System.Console.WriteLine("You chose option \"2\".");
+        Console.ReadLine();
+    }
+    else if (userSelection == 3)
+    {
+        System.Console.WriteLine("You chose option \"3\".");
+        Console.ReadLine();
+    }
+    
+}
