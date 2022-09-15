7aef95bd40fb3c162c00298241c77090355ab1f6
Evan <112584442+evanpaddock@users.noreply.github.com>
2022-09-15 00:09:34 -0500

    Menu Working

--- Program.cs
+++ Program.cs
@@ -3,50 +3,37 @@ string userName = GetUserName();
  
 int userSelection = SelectionMenu();
 
-int controlInt = UserSelectionVerify();
-
+UserSelectionVerify(userSelection, userName);
+ 
 //methods
 static string GetUserName()
 {
     System.Console.WriteLine("What is your name?");
     string userName = Console.ReadLine();
     System.Console.WriteLine($"Hello {userName} what would you like to do today?");
+    
     return(userName);
 }
 static int SelectionMenu()
 {
-    System.Console.WriteLine("Press \"1\" for Budget Calulator | Press \"2\" for Currency Converter | Press \"3\" for Budget Calulator. Then press enter.");
+    System.Console.WriteLine("");
+    System.Console.WriteLine("Press \"1\" for Budget Calulator");
+    System.Console.WriteLine("Press \"2\" for Currency Converter");
+    System.Console.WriteLine("Type \"Exit\" to exit");
+    System.Console.WriteLine("");
+    System.Console.WriteLine("Then press enter");
+
     int userSelection = int.Parse(Console.ReadLine());
+    
     return (userSelection);
 }
-static int UserSelectionVerify(int userSelection)
+static void UserSelectionVerify(int userSelection, string userName)
 {
-    System.Console.WriteLine($"You chose {userSelection} is this correct? If not enter -1");
+    System.Console.WriteLine($"You chose {userSelection} is this correct? 0 for yes, -1 for no");
     int controlInt = int.Parse(Console.ReadLine());
-    return(controlInt);
+    if( controlInt == -1)
+    {
+        System.Console.WriteLine($"{userName} please try again");
+    }
+    SelectionMenu();
 }
-/*  static void DisplayMenu()
-{
-
-    if(controlInt == -1 )
-    {
-        System.Console.WriteLine("Please try again!");
-    }
-    else if (userSelection == 1)
-    {
-        System.Console.WriteLine("You chose option \"1\".");
-        Console.ReadLine();
-    }
-    else if (userSelection == 2)
-    {
-        System.Console.WriteLine("You chose option \"2\".");
-        Console.ReadLine();
-    }
-    else if (userSelection == 3)
-    {
-        System.Console.WriteLine("You chose option \"3\".");
-        Console.ReadLine();
-    }
-    
-}
-*/
